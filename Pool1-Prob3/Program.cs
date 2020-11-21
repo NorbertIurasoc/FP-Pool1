e5ce456307914721bede4c492abd3e81f1ec2771
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-21 16:40:31 +0100

    putin code

---
+++ Pool1-Prob3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            if (n % k == 0)
+            {
+                Console.WriteLine("Se divide.");
+
+            }
+            else
+            {
+                Console.WriteLine("Nu se divide.");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
