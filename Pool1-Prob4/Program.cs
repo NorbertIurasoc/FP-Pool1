b2c1867f3ef589666e6abe892a4a55b89f05d13d
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-21 17:44:18 +0100

    putin code

---
+++ Pool1-Prob4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Introduceti anul:");
+            year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))
+            {
+                Console.WriteLine("Este an bisect!");
+            }
+            else
+            {
+                Console.WriteLine("Nu este an bisect!" );
+            }
+            Console.ReadKey();
+        }
+    }
+}
