14562d5cc6ef80f532879095e16257cb382fe235
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-22 15:04:47 +0100

    putin code iarasi

---
+++ Pool1-Prob8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Introduceti a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti b: ");
+            b = int.Parse(Console.ReadLine());
+
+            b = (a + b) - (a = b);
+
+            Console.WriteLine("a = " + a + " " + "b = " + b);
+
+
+            Console.ReadKey();
+        }
+    }
+}
