9d2f6add226ccac2505b3c8c7992fdacf8973b52
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-22 14:53:53 +0100

    nu prea greu

---
+++ Pool1-Prob7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, aux;
+
+            Console.WriteLine("Introduceti a: ");
+            a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Introduceti b: ");
+            b = int.Parse(Console.ReadLine());
+            
+            aux = a; a = b; b = aux;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            Console.ReadKey();
+        }
+    }
+}
