ee9025e106c2f7f5e65afd86bf258e33e538577d
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-22 14:35:58 +0100

    putin code

---
+++ Pool1-Prob6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+        
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Console.WriteLine("Pot fi laturile unui triunghi!");
+            }
+            else
+            {
+                Console.WriteLine("Nu pot fi laturile unui triunghi!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
