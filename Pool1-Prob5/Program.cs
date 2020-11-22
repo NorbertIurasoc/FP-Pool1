778af347660a34da7df3e4173cc5564c126858b9
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-22 14:28:15 +0100

    nu stiu

---
+++ Pool1-Prob5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            Console.WriteLine("Introduteti numarul: ");
+
+            n = int.Parse(Console.ReadLine());
+            
+
+            
+                
+                while (n > 9)
+            {
+                n = n % 10;
+                
+            }
+
+            Console.WriteLine(n);
+
+
+            Console.ReadKey();
+        }
+    }
+}
