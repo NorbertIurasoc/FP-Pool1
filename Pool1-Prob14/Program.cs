161c4dd481991178beb949d5d1136f351b9b4dfd
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-11-24 15:49:13 +0100

    putin code

---
+++ Pool1-Prob14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool1_Prob14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int n, a, b;
+			a = 0;
+            Console.WriteLine("n = ");
+			n = int.Parse(Console.ReadLine());
+			b = n;
+			while (n > 0)
+			{
+				a = a * 10 + n % 10;
+				n = n / 10;
+			}
+			if (a == b)
+			{
+				Console.WriteLine("Este palindrom!");
+			}
+			else
+			{
+                Console.WriteLine("Nu este palindrom!");
+			}
+
+			Console.ReadKey();
+		}
+    }
+}
